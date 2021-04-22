dc2820d77d70be043d17fa31f4c5c0c3082898ad
Tom Smith <74121145+TomSmith98-C@users.noreply.github.com>
2021-04-22 14:16:56 -0500

    Add files via upload

---
+++ Finally_en_CSharp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Finally_en_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                Console.WriteLine("Ingresa un numero.");
+                int numero;
+            try
+            {
+                numero = int.Parse(Console.ReadLine());
+
+                }
+
+            catch (Exception e)
+            {
+                Console.WriteLine("No es valido el numero  ");
+            }
+
+            //Finally se usa para poner todo lo que quieras que se ejecute aun si manda una excepcion el programa,
+            //Siempre se ejecuta con o sin excepciones
+
+            finally
+            {
+                numero = 0;
+            }
+
+            Console.WriteLine($"Salto a seccion finally donde el numero es igual a =  {numero}.");
+        }
+    }
+}
